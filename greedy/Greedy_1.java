559c7bc950f6c1974b55fbde8179ab71c1d39718
YJ.SONG <yj.song@renocomms.com>
2025-02-20 10:29:09 +0900

    first commit

---
+++ greedy/Greedy_1.java
@@ -0,0 +1,52 @@
+/* 
+***문제***
+[문제 출처-프로그래머스 코딩테스트 고득점 Kit]
+
+점심시간에 도둑이 들어, 일부 학생이 체육복을 도난당했습니다. 
+다행히 여벌 체육복이 있는 학생이 이들에게 체육복을 빌려주려 합니다. 
+학생들의 번호는 체격 순으로 매겨져 있어, 바로 앞번호의 학생이나 바로 뒷번호의 학생에게만 체육복을 빌려줄 수 있습니다. 
+예를 들어, 4번 학생은 3번 학생이나 5번 학생에게만 체육복을 빌려줄 수 있습니다. 
+체육복이 없으면 수업을 들을 수 없기 때문에 체육복을 적절히 빌려 최대한 많은 학생이 체육수업을 들어야 합니다.
+
+전체 학생의 수 n, 체육복을 도난당한 학생들의 번호가 담긴 배열 lost, 
+여벌의 체육복을 가져온 학생들의 번호가 담긴 배열 reserve가 매개변수로 주어질 때, 
+체육수업을 들을 수 있는 학생의 최댓값을 return 하도록 solution 함수를 작성해주세요. */
+
+
+import java.util.*;
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        
+        Arrays.sort(reserve);
+        Arrays.sort(lost);
+        
+        // 체육복을 입을 수 있는 최소한의 학생 수
+        answer = n - lost.length;
+        
+        // 여벌 체육복이 있지만 자신도 도난당한 학생 수
+        for (int i = 0; i < lost.length; i++) {
+			for (int j = 0; j < reserve.length; j++) {
+				if (lost[i] == reserve[j]) {
+					answer++;
+					lost[i] = -1;
+					reserve[j] = -1;
+                    break;
+				}
+			}
+		}
+        
+        // 도난당했지만 체육복을 빌릴 수 있는 학생 수
+        for (int i = 0; i < lost.length; i++) {
+			for (int j = 0; j < reserve.length; j++) {
+				if (lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]) {
+					answer++;
+					reserve[j] = -1;
+					break;
+				}
+			}
+		}
+        
+        return answer;
+    }
+}
